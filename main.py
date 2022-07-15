2a9460998eb35abf06d8ee82ac37bc8ec8e6274b
Brayant Flores <brayant71@gmail.com>
2022-07-15 04:20:06 -0500

    project

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 #New project for Brayant Flores
+# Programa que tenga dos secciones para registrar productos o venderlos y ticket
+# catalago creado por el usuario 
+# y en la venta agregar lo que esta en el catalogo e imprimir el ticket con iva
