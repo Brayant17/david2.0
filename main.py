784600bc39425ad190efcdab2f7d152a3c46775a
Brayant Flores <brayant71@gmail.com>
2022-07-16 01:09:05 -0500

    files olvidado

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 # Programa que tenga dos secciones para registrar productos o venderlos y ticket
 # catalago creado por el usuario 
 # y en la venta agregar lo que esta en el catalogo e imprimir el ticket con iva
-from clases import Producto
+from clases import Productos
 
-producto = clases.Productos("refresco", "Refresco de 1lt", "01021201", 35)
+producto = Productos("refresco", "Refresco de 1lt", "01021201", 35)
 
 print("Prodcuto",  "Descripcion", "\t ", "\tPrecio")
 producto.set()
